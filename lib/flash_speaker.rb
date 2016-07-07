02f182294809ef4ac76b5caf4376a32ca9d55f96
Kris <elhowm@gmail.com>
2016-07-07 17:23:01 +0300

    add translation params

--- lib/flash_speaker.rb
+++ lib/flash_speaker.rb
@@ -1,21 +1,21 @@
 module FlashSpeaker
-  def attach_message(success_flag, entity_errors = nil)
+  def attach_message(success_flag, entity_errors = nil, t_params = {})
     flash_type = success_flag ? :success : :failed
     flash[:errors] = entity_errors if entity_errors.present?
-    flash[flash_type] = message(flash_type)
+    flash[flash_type] = message(flash_type, t_params)
   end
 
   private
 
-  def message(flash_type)
+  def message(flash_type, t_params = {})
     case
     when message_exists?(message_path(flash_type))
-      t message_path(flash_type)
+      t message_path(flash_type), t_params
     when message_exists?(default_message_path(flash_type))
-      msg = t default_message_path(flash_type)
+      msg = t default_message_path(flash_type), t_params
       msg.sub('<entity>', entity_name)
     else
-      raise "translation missing: #{message_path}"
+      raise "translation missing: #{message_path(flash_type)}"
     end
   end
 
